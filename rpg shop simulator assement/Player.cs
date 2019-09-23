169dacf41a7fc133ab2036cecd593bf4156434dd
Reid Eastin <s198012@students.aie.edu>
2019-09-23 14:38:07 -0500

    Add project files.

---
+++ rpg shop simulator assement/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_shop_simulator_assement
+{
+    class Player
+    {
+        int gold;
+
+        
+    }
+}
---
+++ rpg shop simulator assement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_shop_simulator_assement
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool shop = true;
+            while(shop)
+            {
+                Console.WriteLine("1: look at shop inventory");
+                Console.WriteLine("2: look at your inventory");
+                Console.WriteLine("3: Save");
+                Console.WriteLine("4: leave");
+                
+            }
+
+        }
+    }
+}
