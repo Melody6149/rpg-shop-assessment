8ea9c7c659e0e3ea7b30f54f01222f40f42db5c3
Reid Eastin <s198012@students.aie.edu>
2019-09-23 17:45:03 -0500

    made some changes I will finish this later

--- rpg shop simulator assement/Player.cs
+++ rpg shop simulator assement/Player.cs
@@ -9,7 +9,19 @@ namespace rpg_shop_simulator_assement
     class Player
     {
         int gold;
-
         
+        public void playerinventory()
+        {
+            
+            Console.WriteLine("Inventory menu");
+            Console.WriteLine();
+            bool inventoryopen = true; // useds for loop to know if inventory is open
+            while (inventoryopen)
+            {   
+                Console.WriteLine();
+                
+
+            } 
+        }
     }
 }
--- rpg shop simulator assement/Program.cs
+++ rpg shop simulator assement/Program.cs
@@ -12,15 +12,28 @@ namespace rpg_shop_simulator_assement
         static void Main(string[] args)
         {
             bool shop = true;
+            string choice = "0";
             while(shop)
             {
                 Console.WriteLine("1: look at shop inventory");
                 Console.WriteLine("2: look at your inventory");
                 Console.WriteLine("3: Save");
                 Console.WriteLine("4: leave");
-                
-            }
 
+               
+                choice = Console.ReadLine();
+
+                if (choice == "2")
+                {
+                    Player.playerinventory();
+                }
+
+                else if (choice == "4")
+                {
+                    shop = false;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
